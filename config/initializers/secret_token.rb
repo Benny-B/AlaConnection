8d7a6fa798454b156b4a6a5c9015337c62cba2b2
Mlekena <mlekena@skidmore.edu>
2013-07-02 10:29:01 +0200

    alacon project started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlaCon::Application.config.secret_token = '9a421e59c7b1ce2f5b40ec6f76627550e700b4dd89914d7a3f30e010594db88de05a05f80333377d54d93f7899b1ef4c24335873f7eb0dad628bff41d10e936a'
